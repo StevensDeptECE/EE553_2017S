e7560bdf7d704c36324ca156e7f773b35378303e
Dov Kruger <dov.kruger@gmail.com>
2017-02-11 08:04:12 -0500

    fixed hw3a and moved everything to session03

--- session03/hw3a.cc
+++ session03/hw3a.cc
@@ -7,10 +7,13 @@ int main() {
 	Vec3d b(5.0);         //(5.0, 0.0, 0.0)
 	Vec3d c = a + b;
 	cout << c << '\n';
-	Vec3d c = a.add(b);   // call a method
+	Vec3d c2 = a.add(b);   // call a method
+	cout << c2 << '\n';
 	double d = a.dot(b); //  dot product = a.x*b.x + a.y*b.y + a.z*b.z
 	cout << d << '\n';
 
 	Vec3d e = a * 2;   //scalar multiplication
+	cout << e << '\n';
 	Vec3d f = 2 * a;   //scalar multiplication
-}	
+	cout << f << '\n';
+}
